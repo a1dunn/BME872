d26d278ad62b3de933958e129fd793d1a74552c2
Alex Dunn <a1dunn@ryerson.ca>
2023-01-19 15:47:45 -0500

    adding intensityHisto code

---
+++ intensityHistogram.m
@@ -0,0 +1,29 @@
+function [bins, freq] =  intensityHistogram(img, slice, mode) % mode 0 = slice, mode 1 = volume
+% Mode 0 is single slice image histogram, uses input parameter slice to
+% determine which slide to determine histogram for
+
+[x, y, z] = size(img);
+freq = zeros(1, 256);
+bins = 0:255;
+
+if mode == 0
+    for i = 1:x
+        for j = 1:y
+            intensity = img(i, j, slice);
+            freq(intensity+1) = freq(intensity+1) + 1;
+        end
+    end
+    % Mode 1 is volume image histogram, dopesnt use input parameter slice
+elseif mode == 1
+    for k = 1:z
+        for i = 1:x
+            for j = 1:y
+                intensity = img(i, j, k);
+                freq(intensity+1) = freq(intensity+1) + 1;
+            end
+        end
+    end
+end
+
+freq = freq/max(freq);
+end
