2c1a8b52de8ba039e9ea6b7f538cf55d2f144a7f
Cassidy Rose <cassidyrose46@hotmail.com>
2023-01-26 15:20:04 -0500

    Updated to include ability to plot subplot of original image

--- plotHist.m
+++ plotHist.m
@@ -1,14 +1,30 @@
-function [] = plotHist(bins,freq, title_plot)
+function [] = plotHist(bins,freq, title_plot, varargin)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-figure 
-hold on
-bar(bins,freq, 'b')
-title(title_plot)
-ylabel('Number of Pixels')
-xlabel('Intensity')
-hold off
+if isempty(varargin) == 0
+    figure
+    hold on
+    imshow(varargin{1,1},[min(min(varargin{1})) max(max(varargin{1}))])
+    colorbar('eastoutside')
+    hold off
+else
+    figure
+    subplot(1,2,1)
+    hold on
+    bar(bins,freq,'b')
+    title('Image')
+    hold off
+    subplot(1,2,2)
+    hold on
+    bar(bins,freq,'b')
+    title('Intensity Histogram')
+    ylabel('Intensity Histogram')
+    xlabel('Intensity')
+    hold off
+    sgtitle(title_plot) 
+end
+
 
 end
 
