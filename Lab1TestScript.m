88da7532183a01c033cddd086ea45979be042934
Cassidy Rose <cassidyrose46@hotmail.com>
2023-02-01 23:46:54 -0500

    Minor updates

--- Lab1TestScript.m
+++ Lab1TestScript.m
@@ -2,21 +2,20 @@ close all
 clear all
 clc
 
-num_MRI_img = 20;
+%%%%%%%%%% 2.1 LOADING, SAVING, AND VIEWING MEDICAL IMAGES %%%%%%%%%%
+
+%% 2.1.1. Question 1: Medical Image Loading and Viewing
 
 folder = 'C:\Users\cassi\OneDrive\Documents\BME 872\Labs\Lab 1\Lab1 - LungCT\Lab1 - LungCT';
 filename = 'training_post';
 imageFormat = '.mhd';
-
 [volCT, infoCT] = imageRead(folder, imageFormat, filename);
 
 folder = 'C:\Users\cassi\OneDrive\Documents\BME 872\Labs\Lab 1\Lab1 - BrainMRI1\Lab1 - BrainMRI1';
 imageFormat = '.dcm';
-
 [~, infoBrain] = imageRead(folder, imageFormat, 'brain_001'); % newly added
 
 for i_frames_brain_MRI = 1:20
-    filename = 'brain_0';
     if i_frames_brain_MRI<10
         filename = strcat('brain_00', num2str(i_frames_brain_MRI));
     else 
@@ -29,13 +28,12 @@ end
 folder = 'C:\Users\cassi\OneDrive\Documents\BME 872\Labs\Lab 1';
 filename = 'PNGTest';
 imageFormat = '.png';
-
 [img_png, info_png] = imageRead(folder, imageFormat, filename);
+
 % folder = 'C:\Users\DunnA\Documents\YEAR4\BME872\872Labz\BrainMRI1';
 % imageFormat = '.dcm';
 
-
-%% Plot a single example of a lung CT image
+% Plot a single example of a lung CT image
 
 figure
 imshow(volCT.data(:,:,volCT.size(3)/2),[min(min(volCT.data(:,:,volCT.size(3)/2))) max(max(volCT.data(:,:,volCT.size(3)/2)))])
@@ -45,24 +43,89 @@ title('Lung CT - Slice 143')
 
 infoBrain.SpacingBetweenSlices
 
-%% Plot a single example of a brain MRI image
+% Plot a single example of a brain MRI image
 
 dimensions_brainMRI = size(volBrain);
 brain_MRI_double  = cast(volBrain(:,:,dimensions_brainMRI(3)/2), "double");
+figure
+imshow(brain_MRI_double,[min(min(brain_MRI_double)) max(max(brain_MRI_double))])
+colorbar('eastoutside')
+clear title
+title('Brain MRI - Slice 10')
 
-% figure
-% imshow(brain_MRI_double,[min(min(brain_MRI_double)) max(max(brain_MRI_double))])
-% colorbar('eastoutside')
-% clear title
-% title('Brain MRI - Slice 10')
+% Plot all brain MRI slices in a single figure
 
-%% Plot all brain MRI slices in a single figure
+figure
+montage(volBrain, 'DisplayRange', [double(min(min(min(volBrain(:,:,:))))) double(max(max(max(volBrain(:,:,:)))))] )
+colorbar('eastoutside')
+clear title
+title('Brain MRI - All Slices')
 
-% figure
-% montage(volBrain, 'DisplayRange', [double(min(min(min(volBrain(:,:,:))))) double(max(max(max(volBrain(:,:,:)))))] )
-% colorbar('eastoutside')
-% clear title
-% title('Brain MRI - All Slices')
+%% 2.1.1. Question 2: Medical Image Writing
+
+sliceLungCT= volCT.data(:,:,volCT.size(3)/2);
+
+img = volBrain;
+filename = 'brain';
+path = 'C:\Users\cassi\OneDrive\Documents\BME 872\Labs\Lab 1\writeImageTest';
+info = infoBrain_all;
+imageWrite(path, '.dcm', img, filename, info)
+
+for i_frames_brain_MRI = 1:20
+    if i_frames_brain_MRI<10
+        filename = strcat('brain_00', num2str(i_frames_brain_MRI));
+    else
+        filename = strcat('brain_0', num2str(i_frames_brain_MRI));
+    end
+    
+    [volBrainread(:,:,i_frames_brain_MRI), infoBrainRead(i_frames_brain_MRI)] = imageRead(path, '.dcm', filename);
+end
+
+if min(min(min(volBrainread == volBrain)))
+    disp('.dcm files were saved successfully with no information loss')
+    dimensions_brainMRI = size(volBrain);
+    
+    brain_MRI_double  = cast(volBrain(:,:,dimensions_brainMRI(3)/2), "double");
+    brain_MRI_double_read  = cast(volBrainread(:,:,dimensions_brainMRI(3)/2), "double");
+    %
+        figure
+        subplot(1,2,1)
+        hold on
+        imshow(brain_MRI_double,[min(min(brain_MRI_double)) max(max(brain_MRI_double))])
+        colorbar('eastoutside')
+        title('Original Brain MRI - Slice 10')
+        hold off
+        subplot(1,2,2)
+        hold on
+        imshow(brain_MRI_double,[min(min(brain_MRI_double)) max(max(brain_MRI_double))])
+        colorbar('eastoutside')
+        title('Saved and Reread Brain MRI - Slice 10')
+        hold off
+    
+end
+
+path = 'C:\Users\cassi\OneDrive\Documents\BME 872\Labs\Lab 1\Lab1 - LungCT\Lab1 - LungCT';
+imageWrite(path, '.png', sliceLungCT, 'imgCT PNG')
+[imgCT_PNG, info] = imageRead(path, '.png', 'imgCT PNG');
+
+figure
+subplot(1,2,1)
+hold on
+imshow(volCT.data(:,:,volCT.size(3)/2),[min(min(volCT.data(:,:,volCT.size(3)/2))) max(max(volCT.data(:,:,volCT.size(3)/2)))])
+colorbar('eastoutside')
+clear title
+title('Original Lung CT - Slice 143')
+hold off
+subplot(1,2,2)
+hold on
+imshow(imgCT_PNG,[min(min(imgCT_PNG)) max(max(imgCT_PNG))])
+colorbar('eastoutside')
+title('Saved as PNG and Reread Lung CT - Slice 143')
+hold off
+
+%%%%%%%%%% 2.2 INTENSITY HISTOGRAMS %%%%%%%%%%
+
+%% Intensity Histograms and Point Operations
 
 % brain and lung signle slice with image
 [bins, freq] = intensityHistogram(volBrain, dimensions_brainMRI(3)/2, 0, 0, 1);
@@ -97,5 +160,3 @@ B = 100;
 out_img = apply_point_tfrm(volBrain(:,:,12), C, B);
 [bins, freq] = intensityHistogram(out_img, 1, 0, 0, 1);
 plotHist(bins,freq, "2.3.1a: Brain 1 MRI Slice 12, C = , B = ", out_img)
-
-close all
--- Lab1TestScript2.m
+++ Lab1TestScript2.m
@@ -1,74 +1,4 @@
-sliceLungCT= volCT.data(:,:,volCT.size(3)/2);
-
-% [bins, freq] = intensityHistogram(volBrain, 10, 0, 0, 0)
-% plotHist(bins,freq, 'Brain', volBrain(:,:,10))
-
-% img = volBrain;
-% filename = 'brain';
-% path = 'C:\Users\cassi\OneDrive\Documents\BME 872\Labs\Lab 1\writeImageTest';
-% info = infoBrain;
-% imageWrite(path, '.dcm', img, filename, info)
-% 
-% for i_frames_brain_MRI = 1:20
-%     if i_frames_brain_MRI<10
-%         filename = strcat('brain_00', num2str(i_frames_brain_MRI));
-%     else
-%         filename = strcat('brain_0', num2str(i_frames_brain_MRI));
-%     end
-%     
-%     [volBrainread(:,:,i_frames_brain_MRI), infoBrainRead(i_frames_brain_MRI)] = imageRead(path, '.dcm', filename);
-% end
-
-% if min(min(min(volBrainread == volBrain)))
-%     disp('.dcm files were saved successfully with no information loss')
-%     dimensions_brainMRI = size(volBrain);
-%     
-%     brain_MRI_double  = cast(volBrain(:,:,dimensions_brainMRI(3)/2), "double");
-%     brain_MRI_double_read  = cast(volBrainread(:,:,dimensions_brainMRI(3)/2), "double");
-%     %
-    %     figure
-    %     subplot(1,2,1)
-    %     hold on
-    %     imshow(brain_MRI_double,[min(min(brain_MRI_double)) max(max(brain_MRI_double))])
-    %     colorbar('eastoutside')
-    %     title('Original Brain MRI - Slice 10')
-    %     hold off
-    %     subplot(1,2,2)
-    %     hold on
-    %     imshow(brain_MRI_double,[min(min(brain_MRI_double)) max(max(brain_MRI_double))])
-    %     colorbar('eastoutside')
-    %     title('Saved and Reread Brain MRI - Slice 10')
-    %     hold off
-    %
-% end
-
-path = 'C:\Users\cassi\OneDrive\Documents\BME 872\Labs\Lab 1\Lab1 - LungCT\Lab1 - LungCT';
-imageWrite(path, '.png', sliceLungCT, 'imgCT PNG')
-[imgCT_PNG, info] = imageRead(path, '.png', 'imgCT PNG');
-
-figure
-subplot(1,2,1)
-hold on
-imshow(volCT.data(:,:,volCT.size(3)/2),[min(min(volCT.data(:,:,volCT.size(3)/2))) max(max(volCT.data(:,:,volCT.size(3)/2)))])
-colorbar('eastoutside')
-clear title
-title('Original Lung CT - Slice 143')
-hold off
-subplot(1,2,2)
-hold on
-imshow(imgCT_PNG,[min(min(imgCT_PNG)) max(max(imgCT_PNG))])
-colorbar('eastoutside')
-title('Saved as PNG and Reread Lung CT - Slice 143')
-hold off
-
-x = [1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16];
-x_uint8 = uint8(x)
-b = uint16(20.*ones(4,4));
-x_uint16 = uint16(x_uint8);
-
-y = 20.*x_uint16 + b
-
-%% 2.3.1 QUESTION 3
+%% 2.3.1 Question 3a) Image Subtraction
 folder = 'C:\Users\cassi\OneDrive\Documents\BME 872\Labs\Lab 1\Lab1 - LungCT\Lab1 - LungCT';
 imageFormat = '.mhd';
 filename ='training_post';
@@ -144,8 +74,7 @@ colorbar('eastoutside')
 title('Subtracted (Perfusion) Lung CT - Slice 200')
 hold off
 
-
-%% Image Subtraction 3b)
+%% 2.3.1 Question 3b) Image Subtraction
 
 folder = 'C:\Users\cassi\OneDrive\Documents\BME 872\Labs\Lab 1\Lab1 - LungCT\Lab1 - LungCT';
 imageFormat = '.mhd';
@@ -192,9 +121,7 @@ for i_slice = [80 143 200]
     hold off
 end
 
-
-
-%% 10x
+%% 2.3.1 Question 3c) Image Subtraction
 
 filename ='noise_10x_post';
 [volCT_noise, ~] = imageRead(folder, imageFormat, filename);
