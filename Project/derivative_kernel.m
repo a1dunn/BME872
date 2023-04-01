51d31c8245a4c8c469f714ddb323e3052b8d8922
Alex Dunn <a1dunn@ryerson.ca>
2023-04-01 02:27:43 -0400

    Starting project

---
+++ Project/derivative_kernel.m
@@ -0,0 +1,48 @@
+function [kernel, filter_title] = derivative_kernel(name, direction)
+%DERIVATIVE_KERNEL Provides a filtering mask for the requested derivative
+%filter
+%   Inputs:
+%       name: a string containing the name of the filtering operation to be performed
+%               - 'central': central difference operation filter kernel
+%               - 'forward': forward difference operation filter kernel
+%               - 'prewitt': Prewitt filter kernel
+%               - 'sobel': Sobel filter kernel
+%       direction: a string containing the desired edge detection direction
+%               - 'x': provide the filter kernel for detecting vertical edges
+%               - 'y': provide the filter kernel for detecting vertical edges
+%   Output:
+%       kernel: requested derivative filter kernel
+%       filter_title: name of the filter for figure title
+
+if strcmp('central',name)
+    filter_title = 'Central Difference';
+    if strcmp(direction,'y')
+        kernel = [1 0 -1];
+    elseif strcmp(direction,'x')
+        kernel = [1 0 -1]';
+    end
+elseif strcmp('forward',name)
+    filter_title = 'Forward Difference';
+    if strcmp(direction,'y')
+        kernel = [0 1 -1];
+    elseif strcmp(direction,'x')
+        kernel = [0 1 -1]';
+    end
+elseif strcmp('prewitt',name)
+    filter_title = 'Prewitt';
+    if strcmp(direction,'y')
+        kernel = [1 0 -1; 1 0 -1; 1 0 -1];
+    elseif strcmp(direction,'x')
+        kernel = [-1 -1 -1; 0 0 0; 1 1 1];
+    end
+elseif strcmp('sobel',name)
+    filter_title = 'Sobel';
+    if strcmp(direction,'y')
+        kernel = [1 0 -1; 2 0 -2; 1 0 -1];
+    elseif strcmp(direction,'x')
+        kernel = [1 2 1; 0 0 0; -1 -2 -1];
+    end
+end
+
+end
+
