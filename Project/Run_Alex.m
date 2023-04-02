d2029e1290ca58326e25f38f8481a2ecf94f420d
Alex Dunn <a1dunn@ryerson.ca>
2023-04-02 02:04:00 -0400

    Updating working edge

--- Project/Run_Alex.m
+++ Project/Run_Alex.m
@@ -1,11 +1,12 @@
 %% Medical Image Loading
-
+clear all 
+close all
 folder = 'C:\Users\DunnA\Documents\YEAR4\BME872\872Labz\LungCT';
 filename = {'training_post', 'noise_0.5x_post', 'noise_10x_post', 'training_pre19mm'};
 imageFormat = '.mhd';
 for filevairations = 1:4
     [volCT, infoCT] = imageRead(folder, imageFormat, char(filename(filevairations)));
-    CT_data(:, :, filevairations) = volCT;
+    CT_data(:, :, :, filevairations) = volCT.data;
 end
 
 folder = 'C:\Users\DunnA\Documents\YEAR4\BME872\872Labz\BrainMRI2';
@@ -13,11 +14,10 @@ folder = 'C:\Users\DunnA\Documents\YEAR4\BME872\872Labz\BrainMRI2';
 for i_frames_brain_MRI = 1:6
     filename = strcat(folder, '\brainMRI_', num2str(i_frames_brain_MRI), '.mat');
     img = load(filename);
-    MRI_data(:, :, i_frames_brain_MRI) = img;
+    MRI_data(:, :, :, i_frames_brain_MRI) = img.vol;
 end
 
 
 %% Calculate Edge Quality
-for filevairations = 1:4
-    imageQuality_edge(CT_data(:, :, filevairations), 'plot');
-end
+
+E = imageQuality_edge(CT_data, 'plot');
--- Project/imageQuality_edge.m
+++ Project/imageQuality_edge.m
@@ -1,17 +1,60 @@
-function [E] = imageQuality_edge(img, plotting)
+function [E] = imageQuality_edge(imgs, plotting)
+% Compute edge quality metric for input images
+% imgs: input 3D image
+% plotting: if 'plot', display edge quality metric plot
+E = zeros(size(imgs, 3), size(imgs, 4));
+for variations = 1:size(imgs, 4)
+    variations
+    %% Gaussian Filtering
+    sigma = 2;
+    % Create 2D Gaussian kernel
+    kernel_size = ceil(6*sigma);
+    if mod(kernel_size, 2) == 0
+        kernel_size = kernel_size + 1;
+    end
+    kernel = fspecial('gaussian', kernel_size, sigma);
 
-h = [1 4 7 4 1; 4 20 33 20 4; 7 33 55 33 7; 4 20 33 20 4; 1 4 7 4 1];
-img_smooth = (1/331).*spatial_filter(img,h);
+    % Apply filter to each slice in image
+    filtered_image = zeros(size(imgs(:, : , :, variations)));
+    for i = 1:size(imgs, 3)
+        filtered_image(:, :, i) = imfilter(imgs(:, :, i, variations), kernel, 'same', 'replicate');
+    end
 
-% Apply x-direction kernel
-[kernel, ~] = derivative_kernel('sobel', 'x');
-grad_x = spatial_filter(img_smooth,kernel);
 
-% Apply y-direction kernel
-[kernel, filter_title] = derivative_kernel('sobel', 'y');
-grad_y = spatial_filter(img_smooth,kernel);
+    sobel_x = [-1 0 1; -2 0 2; -1 0 1];
+    sobel_y = [-1 -2 -1; 0 0 0; 1 2 1];
+    threshold = 50;
+    
+    for i = 1:size(imgs, 3)
+        % Apply the Sobel kernels
+        dx = conv2(imgs(:, : , i, variations), sobel_x, 'same');
+        dy = conv2(imgs(:, : , i, variations), sobel_y, 'same');
 
-% Calculate gradient magnitude
-grad_mag = (grad_y.^2 + grad_x.^2).^(1/2);
+        % Calculate gradient magnitude
+        grad_mag(:, :, i) = (dy.^2 + dx.^2).^(1/2);
+        sum_above_thresh = sum(grad_mag(grad_mag > threshold));
+        num_above_thresh = sum(grad_mag(:) > threshold);
+        % Compute edge quality metric for each slice in filtered image
+        E(i, variations) = sum_above_thresh/num_above_thresh;
+    end
 
+
+    
+
+
+    
+
+end
+
+% Display edge quality metric plot if requested
+if strcmp(plotting, 'plot')
+    figure;
+    % Create a scatter plot with small dots
+    scatter(repelem(1:4, 286), E(:), 1, 'filled');
+
+    % Set the axis labels and title
+    xlabel('Slice number');
+    ylabel('Edge quality');
+    title('Edge quality metric');
+end
 end
