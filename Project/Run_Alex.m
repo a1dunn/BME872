eeb9f22cbfe94322f9ad5abaaa02000ffb345aa7
Alex Dunn <a1dunn@ryerson.ca>
2023-04-03 11:36:22 -0400

    Done edge

--- Project/Run_Alex.m
+++ Project/Run_Alex.m
@@ -17,7 +17,86 @@ for i_frames_brain_MRI = 1:6
     MRI_data(:, :, :, i_frames_brain_MRI) = img.vol;
 end
 
+[ct_row, ct_col, ct_sli] = size(double(CT_data(:, :, :, 1)));
+[mr_row, mr_col, mr_sli] = size(double(MRI_data(:, :, :, 1)));
 
 %% Calculate Edge Quality
+for CTfiles = 1:4
+    for slice_num = 1:ct_sli
+      
+        E_CT(CTfiles, slice_num) = imageQuality_edge(CT_data(:, :, slice_num, CTfiles),2, 10);
+        
+    end
+end
+%%
+for MRfiles = 1:6
+    for slice_num = 1:mr_sli
+        E_MR(MRfiles, slice_num) = imageQuality_edge(MRI_data(:, :, slice_num, MRfiles), 2, 10);
+        
+    end
+end
 
-E = imageQuality_edge(CT_data, 'plot');
+ground_truth_CT = [repmat(1,1,ct_sli); repmat(0.5,1,ct_sli); repmat(10,1,ct_sli); repmat(1,1,ct_sli)];
+% Display edge quality metric plot
+figure;
+hold on
+plot(ground_truth_CT(1, :),E_CT(1, :),'.', 'Color', [10 162 173]./255)
+plot(ground_truth_CT(2, :),E_CT(2, :),'.', 'Color', [1 154 72]./255)
+plot(ground_truth_CT(3, :),E_CT(3, :),'.', 'Color', [32 0 177]./255)
+plot(ground_truth_CT(4, :),E_CT(4, :),'.', 'Color', [206 13 103]./255)
+title('Edge Metric Versus Increasing Noise: Lung CT')
+xlabel('Noise Level')
+ylabel('Edge Metric')
+legend('training post', 'noise 0.5x post', 'noise 10x post', 'training pre19mm')
+hold off
+
+ground_truth_MR = [repmat(1,1,mr_sli); repmat(2,1,mr_sli); repmat(3,1,mr_sli); repmat(4,1,mr_sli); repmat(5,1,mr_sli); repmat(6,1,mr_sli)];
+% Display edge quality metric plot
+figure;
+hold on
+plot(ground_truth_MR(1, :),E_MR(1, :),'.', 'Color', [10 162 173]./255)
+plot(ground_truth_MR(2, :),E_MR(2, :),'.', 'Color', [1 154 72]./255)
+plot(ground_truth_MR(3, :),E_MR(3, :),'.', 'Color', [32 0 177]./255)
+plot(ground_truth_MR(4, :),E_MR(4, :),'.', 'Color', [206 13 103]./255)
+plot(ground_truth_MR(5, :),E_MR(5, :),'.', 'Color', [206 13 103]./255)
+plot(ground_truth_MR(6, :),E_MR(6, :),'.', 'Color', [206 13 103]./255)
+title('Edge Metric Versus Increasing Noise: Brain MRI 2')
+xlabel('Noise Level')
+ylabel('Edge Metric')
+hold off
+
+
+%% Edge quality versus MA
+figure;
+hold on
+for kernal_size = 3:2:30
+    % Moving average to fill vessels
+    kernel_size = [kernal_size, kernal_size];
+    kernel = ones(kernel_size) / prod(kernel_size);
+    for slice_num = 1:2:ct_sli
+        E_MA_CT(kernal_size, slice_num) = imageQuality_edge(imfilter(CT_data(:, :, slice_num, 1), kernel, 'replicate'), 2, 10);
+        plot((kernal_size),E_MA_CT(kernal_size, slice_num), '.', 'Color', [10 162 173]./255)
+    end
+end
+xlabel('Kernal Size (NxN)')
+ylabel('Edge Metric')
+title('Edge Metric Versus Moving Average Kernal Size: Lung CT Training Post')
+hold off
+
+%% Edge quality versus MA
+figure;
+hold on
+for kernal_size = 3:2:30
+    % Moving average to fill vessels
+    kernel_size = [kernal_size, kernal_size];
+    kernel = ones(kernel_size) / prod(kernel_size);
+    for slice_num = 1:2:mr_sli
+        E_MA_MR(kernal_size, slice_num) = imageQuality_edge(imfilter(MRI_data(:, :, slice_num, 1), kernel, 'replicate'), 2, 10);
+        plot((kernal_size),E_MA_MR(kernal_size, slice_num), '.', 'Color', [10 162 173]./255)
+    end
+
+end
+xlabel('Kernal Size (NxN)')
+ylabel('Edge Metric')
+title('Edge Metric Versus Moving Average Kernal Size: Brain MRI 2')
+hold off
--- Project/imageQuality_edge.m
+++ Project/imageQuality_edge.m
@@ -1,60 +1,36 @@
-function [E] = imageQuality_edge(imgs, plotting)
+function [E] = imageQuality_edge(img, sigma, threshold)
 % Compute edge quality metric for input images
 % imgs: input 3D image
 % plotting: if 'plot', display edge quality metric plot
-E = zeros(size(imgs, 3), size(imgs, 4));
-for variations = 1:size(imgs, 4)
-    variations
-    %% Gaussian Filtering
-    sigma = 2;
-    % Create 2D Gaussian kernel
-    kernel_size = ceil(6*sigma);
-    if mod(kernel_size, 2) == 0
-        kernel_size = kernel_size + 1;
-    end
-    kernel = fspecial('gaussian', kernel_size, sigma);
-
-    % Apply filter to each slice in image
-    filtered_image = zeros(size(imgs(:, : , :, variations)));
-    for i = 1:size(imgs, 3)
-        filtered_image(:, :, i) = imfilter(imgs(:, :, i, variations), kernel, 'same', 'replicate');
-    end
-
-
-    sobel_x = [-1 0 1; -2 0 2; -1 0 1];
-    sobel_y = [-1 -2 -1; 0 0 0; 1 2 1];
-    threshold = 50;
-    
-    for i = 1:size(imgs, 3)
-        % Apply the Sobel kernels
-        dx = conv2(imgs(:, : , i, variations), sobel_x, 'same');
-        dy = conv2(imgs(:, : , i, variations), sobel_y, 'same');
-
-        % Calculate gradient magnitude
-        grad_mag(:, :, i) = (dy.^2 + dx.^2).^(1/2);
-        sum_above_thresh = sum(grad_mag(grad_mag > threshold));
-        num_above_thresh = sum(grad_mag(:) > threshold);
-        % Compute edge quality metric for each slice in filtered image
-        E(i, variations) = sum_above_thresh/num_above_thresh;
-    end
-
-
-    
-
-
-    
-
+%% Gaussian Filtering
+% Create 2D Gaussian kernel
+kernel_size = ceil(6*sigma);
+if mod(kernel_size, 2) == 0
+    kernel_size = kernel_size + 1;
 end
+kernel = fspecial('gaussian', kernel_size, sigma);
 
-% Display edge quality metric plot if requested
-if strcmp(plotting, 'plot')
-    figure;
-    % Create a scatter plot with small dots
-    scatter(repelem(1:4, 286), E(:), 1, 'filled');
+img = img - min(min(img));
+img = 256*img/max(max(img));
+% Apply filter to each slice in image
+filtered_image = zeros(size(img));
 
-    % Set the axis labels and title
-    xlabel('Slice number');
-    ylabel('Edge quality');
-    title('Edge quality metric');
-end
+filtered_image(:, :) = imfilter(img, kernel, 'same', 'replicate');
+
+
+sobel_x = [-1 0 1; -2 0 2; -1 0 1];
+sobel_y = [-1 -2 -1; 0 0 0; 1 2 1];
+
+
+% Apply the Sobel kernels
+dx = conv2(filtered_image, sobel_x, 'same');
+dy = conv2(filtered_image, sobel_y, 'same');
+
+% Calculate gradient magnitude
+grad_mag(:, :) = (dy.^2 + dx.^2).^(1/2);
+
+sum_above_thresh = sum(grad_mag(grad_mag > threshold));
+num_above_thresh = sum(grad_mag(:) > threshold);
+% Compute edge quality metric for each slice in filtered image
+E = sum_above_thresh/num_above_thresh;
 end
